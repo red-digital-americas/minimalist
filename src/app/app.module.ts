15f50487d78a65ac1b16c5d67cde31c516bec659
Mac <mac@MacBook-Pro-de-Mac.local>
2019-02-01 14:50:10 -0600

    Subo el componente footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { PagesComponent } from './components/pages/pages.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    PagesComponent
+    PagesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
